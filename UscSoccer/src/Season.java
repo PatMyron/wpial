586752879b8aa2f10b9616fee4290b5cb9c8153f
Pat Myron <pmyron09@gmail.com>
2017-02-28 18:46:20 -0500

    cleanup

--- UscSoccer/src/Season.java
+++ UscSoccer/src/Season.java
@@ -1,10 +1,7 @@
-import java.io.PrintWriter;
-
 class Season extends SeasonTemplate {
 	private final int year;
 
 	Season(int year_) {
-		year = year_;
 		GP = 0;
 		w = 0;
 		t = 0;
@@ -12,22 +9,6 @@ class Season extends SeasonTemplate {
 		GF = 0;
 		GA = 0;
 		goalDifferential = 0;
-	}
-
-	public void printSeasonToTable(PrintWriter writer) {
-		writer.println("<tr>");
-
-		if (year != -1)
-			writeDataToHTMLTable(writer, new Object[]{year, GP, w, t, l, GF, GA, goalDifferential});
-		else
-			writeDataToHTMLTable(writer, new Object[]{"TOTAL", GP, w, t, l, GF, GA, goalDifferential});
-
-		writer.println("<td>");
-		writer.printf("%5.1f %%", winPct);
-		writer.println("</td>");
-
-		writeDoublesToHTMLTable(writer, new double[]{goalsForPerGame, goalsAgainstPerGame, goalDiffPerGame});
-
-		writer.println("</tr>");
+		year = year_;
 	}
 }
